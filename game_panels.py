264dad9c64c3322cf06cedc5a7da987b2f3f4fc3
Wartorle Windows-11 <xcatcherx@gmail/com>
2024-12-05 14:56:06 +0300

    delete functions: classes, info and minimap

--- game_panels.py
+++ game_panels.py
@@ -14,18 +14,6 @@ S_BTN_INFO = 'body > div.game-bg > div > div.position_bottom_right.actions.panel
 S_BTN_MINIMAP = 'body > div.game-bg > div > div.position_bottom_right.actions.panel > button:nth-child(5)'
 
 
-def classes(browser):
-    classes_button = wait_for_clickable(browser, By.CSS_SELECTOR, S_BTN_CLASSES, timeout=10)
-    classes_button.click()
-
-def info(browser):
-    info_button = wait_for_clickable(browser, By.CSS_SELECTOR, S_BTN_INFO, timeout=10)
-    info_button.click()
-
-def minimap(browser):
-    minimap_button = wait_for_clickable(browser, By.CSS_SELECTOR, S_BTN_MINIMAP, timeout=10)
-    minimap_button.click()
-
 def open_modal(browser, btn_selector, modal_selector, modal_name):    
     btn = wait_for_clickable(browser, By.CSS_SELECTOR, btn_selector, timeout=10)
     btn.click() 
