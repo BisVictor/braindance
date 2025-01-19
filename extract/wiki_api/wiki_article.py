4ee470a38472f85b19a8f9c71f6b9f8fc1416e4e
Wartorle Windows-11 <xcatcherx@gmail/com>
2025-01-19 10:22:17 +0300

    add wiki_api and wiki_soup

--- extract/wiki_api/wiki_article.py
+++ extract/wiki_api/wiki_article.py
@@ -4,18 +4,31 @@ import wikipedia
 def get_article(word):
     wikipedia.set_lang("ru")
     page2 = wikipedia.page(word)
-    user_agent = "MyWikipediaApp/1.0 (xcatcherx@gmail.com)"  # Замените на свои данные
+    user_agent = "MyWikipediaApp/1.0 (email@gmail.com)" 
     wiki = wikipediaapi.Wikipedia(
         language="ru", 
         user_agent=user_agent
     )    
     page = wiki.page(word)
+    image_url = None
     if page.exists():
         print(f"Всё ОК. Мы на нужном URL. Запрос: {word}")
+        #print(page.title)
+        page_text = page.text
+        page_text_split = page_text.split('\n')
+        #print(page_text_split[0])
+        
         image_url = page2.images
-        print(image_url[0])
+        #print(image_url[0])
         
     else:
         print(f"Мы не на нужной странице! Запрос: {word}")
+    
+    json_object = {"title": page.title,
+                        "img_src": image_url[0],
+                        "text": page_text_split[0]} # возвращает первую картинку
+    return json_object
 
-get_article("Химия")
+    
+
+get_article("физика")
--- extract/wiki_soup/wiki_article.py
+++ extract/wiki_soup/wiki_article.py
@@ -5,28 +5,30 @@ import re
 
 url = "https://ru.wikipedia.org/wiki/"
 
-#word = input()
-#word = "физика"
-encoding_word = urllib.parse.quote(word)
+#encoded_word = urllib.parse.quote(word)
 
-response = requests.get(url+encoding_word)
-
-src_image = None
-
-soup = BeautifulSoup(response.text, 'html.parser')
-title = soup.find(class_="mw-page-title-main")
-first_paragraph = soup.find('p')
-table = soup.find('table', class_=lambda x: x and x.startswith('infobox infobox-'))
-if table:
-    image = table.find('img')
-    print(image)
-   
-    if image:        
-        src_image = image['src']
-        print(src_image)
-
-
-print(title.text)
-print(first_paragraph.text)
-if src_image:
-    print(src_image)
+def get_article(word):
+    encoding_word = urllib.parse.quote(word)
+    response = requests.get(url+encoding_word)
+    if response.status_code == 200:
+        print(f"Всё ОК. Мы на нужном URL. Запрос: {word}")
+        src_url = None
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.find(class_="mw-page-title-main")
+        paragraph = soup.find('p')
+        table = soup.find('table', class_=lambda x: x and x.startswith('infobox infobox-'))
+        if table:
+            image = table.find('img')                   
+            if image:        
+                src_url = image['src']                
+        print(title.text)
+        print(paragraph.text)
+        if src_url:
+            print(src_url)
+    else:
+        print(f"Мы не на нужной странице! Запрос: {word}")
+    
+    json_object = {"title": title.text,
+                        "img_src": src_url,
+                        "text": paragraph.text} 
+    return json_object
