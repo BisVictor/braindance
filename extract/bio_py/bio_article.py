9389dbb2447772460839e2d6f9037840b79ae9ea
Wartorle Windows-11 <xcatcherx@gmail/com>
2025-02-06 21:23:51 +0300

    add bio_article

---
+++ extract/bio_py/bio_article.py
@@ -0,0 +1,27 @@
+from Bio import Entrez
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+EMAIL = os.load_dotenv('EMAIL')
+
+
+def get_article(word):
+    Entrez.email = EMAIL    
+    handle = Entrez.esearch(db="pubmed", term=word, retmax=1)
+    record = Entrez.read(handle)
+    handle.close()
+    article_id = record["IdList"][0]
+    handle = Entrez.efetch(db="pubmed", id=article_id, retmode="xml")
+    records = Entrez.read(handle)
+    handle.close()
+
+    article_title = records["PubmedArticle"][0]["MedlineCitation"]["Article"]["ArticleTitle"]
+    article_abstract = records["PubmedArticle"][0]["MedlineCitation"]["Article"]["Abstract"]["AbstractText"][0]
+    article_doi = records["PubmedArticle"][0]["PubmedData"]["ArticleIdList"][1]
+
+    json_object = {"title": article_title,
+                        "img_src": None,
+                        "text": f"{article_abstract} DOI:{article_doi}"}   
+    return json_object
