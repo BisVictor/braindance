6ab265db3fb2fbc66f9f9ae9298decd6d44f79c5
Wartorle Windows-11 <xcatcherx@gmail/com>
2025-03-05 23:44:20 +0300

    add sql, add docker-compose

--- etl.py
+++ etl.py
@@ -4,9 +4,9 @@ from selenium import webdriver
 import time
 
 
-#from extract.wiki.wiki_article import get_article
+from extract.wiki.wiki_article import get_article
 #from extract.google_api.google_books import get_article
-from extract.bio_py.bio_article import get_article
+#from extract.bio_py.bio_article import get_article
 from transform.wiki_brain import from_wiki_articles_to_steps
 from load.braindance.steps import execute_steps_with_data
 from load.braindance.steps import wrap_with_game_creation, execute_steps_with_data, insert_moves
@@ -20,8 +20,8 @@ with webdriver.Chrome() as browser:
     browser.set_window_size(1400, 800)
     
     articles = []    
-    articles.append(get_article("AAV physics")) #for bio_py.bio_article
-    #articles.append(get_article("химия"))
+    #articles.append(get_article("AAV physics")) #for bio_py.bio_article
+    articles.append(get_article("химия"))
     #articles.append(get_article("Физика"))
     #articles.append(get_article("география"))    
     
@@ -31,7 +31,7 @@ with webdriver.Chrome() as browser:
     steps = wrap_with_game_creation(turn_steps)
     #print(steps)
     steps_and_moves = insert_moves(steps)
-    #print(steps_and_moves)
+    print(steps_and_moves)
     execute_steps_with_data(browser, steps_and_moves)
     time.sleep(15)
     
